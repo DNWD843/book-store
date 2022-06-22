c15bc136a998ed4eeff79767c891e852b7026443
Dmitry Neklyudov <dima.kazanec.394@yandex.ru>
2022-06-22 16:41:28 +0300

    initial commit3: general project preferences

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ import { store } from './app/store';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+// const container = document.getElementById('root')!;
+const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
