6d2838fa258d4db6129a24b38c8c91c1c8c9b85c
dneklyudov <dneklyudov@i-novus.ru>
2022-12-08 07:38:36 +0300

    87: удалил useInintialData

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './App.css';
+import { ONE_DAY_TIMESTAMP } from '../../constants';
 import { EFetchStatuses } from '../../enums';
-import { useInitialData } from '../../hooks/useInitialData';
 import { withReduxStore } from '../../provider/withReduxStore';
-import { useAppSelector } from '../../redux/hooks';
-import { selectAuthStatus, selectBooksFetchingStatus } from '../../redux/store';
+import { useAppSelector, useAppDispatch } from '../../redux/hooks';
+import { authActions } from '../../redux/slices/authSlice';
+import { booksActions } from '../../redux/slices/booksSlice';
+import { selectAuthStatus, selectBooksFetchingStatus, serviceActions } from '../../redux/store';
+import { getBooks } from '../../redux/thunks';
+import { IBooksCollection, TUserData } from '../../types';
+import { checkNeedToDataUpdate, storage, storageKeys } from '../../utils';
 import { ContentLoader } from '../ContentLoader';
 
 import { App } from './App';
 
 const AppComponent: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { setUserToStore } = authActions;
+  const { setBooksToStore } = booksActions;
   const authStatus = useAppSelector(selectAuthStatus);
   const booksStatus = useAppSelector(selectBooksFetchingStatus);
 
-  useInitialData({ authStatus, booksStatus });
+  useEffect(() => {
+    dispatch(serviceActions.getUserInfo);
+    const savedUser = storage.getData<TUserData>(storageKeys.USER);
+
+    dispatch(serviceActions.getBooks);
+    const savedBooks = storage.getData<IBooksCollection>(storageKeys.BOOKS);
+
+    if (savedUser) {
+      dispatch(setUserToStore(savedUser));
+    }
+
+    if (!savedBooks
+      || (savedBooks.books && savedBooks.updatedAt && checkNeedToDataUpdate({ date: savedBooks.updatedAt, limit: ONE_DAY_TIMESTAMP }))) {
+      dispatch(getBooks()).then((res) => {
+        dispatch(serviceActions.setBooks);
+        storage.setData(storageKeys.BOOKS, res.payload);
+      })
+        .catch((err) => { console.error(err); });
+    } else {
+      dispatch(setBooksToStore(savedBooks));
+    }
+  }, [dispatch, setBooksToStore, setUserToStore]);
 
   if (authStatus === EFetchStatuses.pending || booksStatus === EFetchStatuses.pending) {
     return (
--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,44 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { ONE_DAY_TIMESTAMP } from '../../constants';
-import { EFetchStatuses } from '../../enums';
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
-import { selectBooksCollection, selectBooksFetchingStatus, selectFetchingDate, serviceActions } from '../../redux/store';
-import { getBooks } from '../../redux/thunks';
-import { checkNeedToDataUpdate, storageKeys, storage } from '../../utils';
+import { useAppSelector } from '../../redux/hooks';
+import { selectBooksCollection } from '../../redux/store';
 import { Card } from '../Card';
-import { ContentLoader } from '../ContentLoader';
 
 import styles from './Cards.module.css';
 
 const CardsComponent: React.FC = () => {
-  const dispatch = useAppDispatch();
   const booksCollection = useAppSelector(selectBooksCollection) || [];
-  const booksFetchingDate = useAppSelector(selectFetchingDate);
-  const fetchBooksStatus = useAppSelector(selectBooksFetchingStatus);
-
-  // useEffect(() => {
-  //   if (booksCollection) {
-  //     storage.setData(storageKeys.BOOKS, { books: booksCollection, updatedAt: booksFetchingDate });
-  //     dispatch(serviceActions.setBooks);
-  //   }
-  // }, [booksCollection, booksFetchingDate, dispatch]);
-
-  if (fetchBooksStatus === EFetchStatuses.pending) {
-    return (<ContentLoader />);
-  }
-
-  // if (!booksCollection) return null;
-
-  // const needsToUpdate = checkNeedToDataUpdate({ date: booksFetchingDate!, limit: ONE_DAY_TIMESTAMP });
-
-  // if (needsToUpdate) {
-  //   dispatch(getBooks()).then((res) => {
-  //     storage.setData(storageKeys.BOOKS, res.payload);
-  //     dispatch(serviceActions.setBooks);
-  //   })
-  //     .catch((err) => { console.error(err); });
-  // }
 
   return (
     <ul className={styles.list}>
