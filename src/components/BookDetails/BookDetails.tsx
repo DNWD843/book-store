3e15e18fa73aa44866db151ab79f5d3a9d2794fc
dneklyudov <dneklyudov@i-novus.ru>
2022-12-10 23:50:26 +0300

    95: добавил флаги в описание книги, настроил иконки и подсказки

--- src/components/BookDetails/BookDetails.tsx
+++ src/components/BookDetails/BookDetails.tsx
@@ -2,13 +2,15 @@ import classNames from 'classnames';
 import React from 'react';
 
 import { RUBLE_SIGN } from '../../constants';
+import { bookmarkActiveIcon, bookmarkIcon } from '../../vendor/icons';
 
 import { TBookDetailsProps } from './BookDetails.props';
 
 import styles from './BookDetails.module.css';
 
 const BookDetails: React.FC<TBookDetailsProps> = (
-  { title, author: { surname, name }, price, genre, cover, description, isAnonymous },
+  { title, author: { surname, name }, price, genre, cover, description,
+    isAnonymous, isAddedToCart, isAddedToFavorites },
 ) => (
   <article className={styles.card}>
     <img alt="book cover" className={styles.coverImage} src={cover} />
@@ -17,19 +19,17 @@ const BookDetails: React.FC<TBookDetailsProps> = (
     <p className={styles.author}>{`${name} ${surname}`}</p>
     {!isAnonymous && (
       <button className={classNames(styles.addToFavoritesButton, 'btn', 'btn-secondary')} type="button">
-        Добавить в избранное
-        <svg
-          className={styles.bookmarkIcon}
-          viewBox="0 0 16 16"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"
-          />
-        </svg>
+        {isAddedToFavorites ? 'Удалить из избранного' : 'Добавить в избранное'}
+        <i className={styles.bookmarkIcon}>{isAddedToFavorites ? bookmarkActiveIcon : bookmarkIcon}</i>
       </button>
     )}
-    <button className={classNames(styles.buyButton, 'btn', 'btn-primary')} type="button">{`Купить за ${price} ${RUBLE_SIGN}`}</button>
+    <button
+      className={classNames(styles.buyButton, 'btn', 'btn-primary')}
+      title={isAddedToCart ? 'Книга добавлена в корзину' : 'Книга будет добавлена в корзину'}
+      type="button"
+    >
+      {isAddedToCart ? 'Убрать из корзины' : `Купить за ${price} ${RUBLE_SIGN}`}
+    </button>
     <h3 className={styles.descriptionTitle}>Описание книги</h3>
     <textarea readOnly className={styles.descriptionText} rows={10} value={description} wrap="soft" />
   </article>
--- src/components/BookDetails/index.tsx
+++ src/components/BookDetails/index.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { NotFoundPage } from '../../pages/NotFoundPage';
 import { useAppSelector } from '../../redux/hooks';
-import { selectBooksCollection, selectUserData } from '../../redux/store';
+import { selectBooksCollection, selectUserData, selectUserSavings } from '../../redux/store';
 import { TUrlParams } from '../../types';
 
 import { BookDetails } from './BookDetails';
 
 const BookDetailsComponent: React.FC = () => {
-  const { bookId } = useParams<TUrlParams>();
+  const { bookId = '' } = useParams<TUrlParams>();
   const booksCollection = useAppSelector(selectBooksCollection);
   const bookInfo = booksCollection?.find((book) => book.id === bookId);
   const { isAnonymous } = useAppSelector(selectUserData);
+
+  const { favorites, cartValue } = useAppSelector(selectUserSavings);
+
+  const isAddedToFavorites = useMemo(() => favorites.includes(bookId), [bookId, favorites]);
+  const isAddedToCart = useMemo(() => cartValue.some((book) => book.id === bookId), [cartValue, bookId]);
 
   if (!bookInfo) {
     return (<NotFoundPage />);
   }
 
-  return (<BookDetails {...bookInfo} isAnonymous={isAnonymous} />);
+  return (
+    <BookDetails
+      {...bookInfo}
+      isAddedToCart={isAddedToCart}
+      isAddedToFavorites={isAddedToFavorites}
+      isAnonymous={isAnonymous}
+    />
+  );
 };
 
 BookDetailsComponent.displayName = 'BookDetailsComponent';
