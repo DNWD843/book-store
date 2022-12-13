6fe7996433da3cb094d5c191ddba3ae74754619d
dneklyudov <dneklyudov@i-novus.ru>
2022-12-13 23:32:17 +0300

    99: сделал редирект с избранного если не залогинен

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -21,29 +21,30 @@ const Profile: React.FC<TProfileProps> = ({ title, onLogout, onDelete, onProfile
         <>
           <img alt="аватар пользователя" className={styles.avatar} src={photoUrl} />
           <button className={classNames('btn btn-outline-secondary btn-sm', styles.menuButton)} type="button" onClick={onProfileClick}>{title}</button>
+
+          <div className={styles.overlay} />
+          <div
+            className={classNames(styles.menu, {
+              [styles.isMenuOpened]: isMenuOpened,
+            })}
+            onMouseLeave={onProfileClick}
+          >
+            <nav className={styles.navLinks}>
+              <li>
+                <Link className={styles.link} to={routes.profile}>Данные профиля</Link>
+              </li>
+              <li>
+                <Link className={styles.link} to={routes.shoppingHistory}>История покупок</Link>
+              </li>
+            </nav>
+            <div className={styles.buttons}>
+              <button className={classNames('btn btn-outline-secondary btn-sm', styles.button)} type="button" onClick={onLogout}>Выйти</button>
+              <button className={classNames('btn btn-outline-danger btn-sm', styles.button)} type="button" onClick={onDelete}>Удалить аккаунт</button>
+            </div>
+          </div>
+
         </>
       )}
-
-    <div className={styles.overlay} />
-    <div
-      className={classNames(styles.menu, {
-        [styles.isMenuOpened]: isMenuOpened,
-      })}
-      onMouseLeave={onProfileClick}
-    >
-      <nav className={styles.navLinks}>
-        <li>
-          <Link className={styles.link} to={routes.profile}>Данные профиля</Link>
-        </li>
-        <li>
-          <Link className={styles.link} to={routes.shoppingHistory}>История покупок</Link>
-        </li>
-      </nav>
-      <div className={styles.buttons}>
-        <button className={classNames('btn btn-outline-secondary btn-sm', styles.button)} type="button" onClick={onLogout}>Выйти</button>
-        <button className={classNames('btn btn-outline-danger btn-sm', styles.button)} type="button" onClick={onDelete}>Удалить аккаунт</button>
-      </div>
-    </div>
   </div>
 );
 
--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Cards } from '../../components/Cards';
 import { Page } from '../../components/Page';
 import { useAppSelector } from '../../redux/hooks';
-import { selectUserSavings } from '../../redux/store';
+import { selectUserData, selectUserSavings } from '../../redux/store';
+import { routes } from '../../routesMap';
 
 export const FavoritesPage: React.FC = () => {
   // FIXME: вернуть тут favorites, когда исправлю слайс
   const { cartValue } = useAppSelector(selectUserSavings);
+  const { isAnonymous } = useAppSelector(selectUserData);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAnonymous) {
+      navigate(routes.books);
+    }
+  }, [isAnonymous, navigate]);
 
   return (
-    <Page title="Избранное">
+    <Page subtitle={!cartValue.length ? 'Вы ничего не выбрали.' : ''} title="Избранное">
       <Cards books={cartValue} />
     </Page>
   );
