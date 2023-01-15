e5ebfb0ce6dfbd7d81466b0c7fca1539da3ad8f1
dneklyudov <dneklyudov@i-novus.ru>
2023-01-15 22:35:03 +0300

    167: fixed styles, added redirect from purchases page

--- src/pages/PurchasesListPage/index.tsx
+++ src/pages/PurchasesListPage/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import isEmpty from 'lodash/isEmpty';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Page } from '../../components/Page';
 import { PurchaseInfoBlockComponent } from '../../components/PurchaseInfoBlock';
 import { useAppSelector } from '../../redux/hooks';
-import { selectUserPurchases } from '../../redux/store';
+import { selectUserData, selectUserPurchases } from '../../redux/store';
+import { routes } from '../../routesMap';
 
 import styles from './PurchasesListPage.module.css';
 
 const PurchasesListPage: React.FC = () => {
   const purchases = useAppSelector(selectUserPurchases);
+  const { isAnonymous } = useAppSelector(selectUserData);
+  const navigate = useNavigate();
+
   const sortedPurchasesInfoMap = Object.entries(purchases)
     .sort((a, b) => {
       if (b[0] > a[0]) {
@@ -17,8 +23,16 @@ const PurchasesListPage: React.FC = () => {
       return -1;
     });
 
+  const subtitle = 'Вы пока ничего не купили.';
+
+  useEffect(() => {
+    if (isAnonymous) {
+      navigate(routes.books);
+    }
+  }, [isAnonymous, navigate]);
+
   return (
-    <Page title="История покупок">
+    <Page subtitle={isEmpty(purchases) ? subtitle : ''} title="История покупок">
       <div className={styles.pageContent}>
         {sortedPurchasesInfoMap.map((purchaseInfo) => (<PurchaseInfoBlockComponent key={purchaseInfo[0]} purchaseInfo={purchaseInfo} />))}
       </div>
