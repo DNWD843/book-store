7efb337c4bd90cd9198641d140dbd2b9333e6687
Dmitry Neklyudov <dima.kazanec.394@yandex.ru>
2023-01-23 10:44:49 +0300

    188: fixed menu positioning and layout

--- src/components/Header/partials/ProfileShortInfo/ProfileShortInfo.tsx
+++ src/components/Header/partials/ProfileShortInfo/ProfileShortInfo.tsx
@@ -4,12 +4,12 @@ import React, { memo } from 'react';
 import styles from './ProfileShortInfo.module.css';
 
 const ProfileShortInfo: React.FC<{ photoUrl: string, title: string }> = ({ photoUrl, title }) => (
-  <>
+  <div className={styles.container}>
     <img alt="аватар пользователя" className={styles.avatar} src={photoUrl} />
     <span className={classNames(styles.title)}>
       {title}
     </span>
-  </>
+  </div>
 );
 
 ProfileShortInfo.displayName = 'ProfileShortInfo';
