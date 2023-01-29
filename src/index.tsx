a75d05454a1cad440a493c8366cd6be0849e0c5b
dneklyudov <dneklyudov@i-novus.ru>
2023-01-30 00:16:08 +0300

    210: pwa: first try

--- src/index.tsx
+++ src/index.tsx
@@ -18,3 +18,9 @@ root.render(
     </ErrorBoundary>
   </BrowserRouter>,
 );
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(() => console.log('service worker registered'))
+    .catch((err) => console.log('service worker not registered', err));
+}
